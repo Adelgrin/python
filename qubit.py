216a30787c302e5dfd87a7b55846e4c2659a7fa5
Adelgrin <50162397+Adelgrin@users.noreply.github.com>
2023-03-21 11:09:40 -0300

    Add files via upload
    qubit sim, that's basically the way a qubit works

--- qubit.py
+++ qubit.py
@@ -1,26 +1,29 @@
 import os
+
 valor = 77
 g = 8
 r = 1
+
+
 def fator(a):
-    dividendo = g**(r/2)+a
+    dividendo = g ** (r / 2) + a
     divisor = valor
     while not dividendo % divisor == 0:
         resultado = dividendo % divisor
         dividendo = divisor
         divisor = resultado
-    dividendo = divisor
-    return dividendo
+    return divisor
+
 while not g ** r % valor == 1:
     r += 1
     print("running {}".format(r))
 os.system('cls')
-print("resultado({})".format(g**r))
+print("resultado({})".format(g ** r))
 print("resto({})".format(g ** r % valor))
-print("fatores ({}^{}/2+1)={}X({}^{}/2-1){}".format(g,r,fator(1),g,r,fator(-1)))
-dividendo1 = fator(1)
-dividendo2 = fator(-1)
-if dividendo1*dividendo2 == valor:
-    print("{}X{}={}".format(dividendo1,dividendo2,valor))
+print("fatores ({}^{}/2+1)={}X({}^{}/2-1){}".format(g, r, fator(1), g, r, fator(-1)))
+fator1 = fator(1)
+fator2 = fator(-1)
+if fator1 * fator2 == valor:
+    print("{}X{}={}".format(fator1, fator2, valor))
 else:
     print("incorreto")
